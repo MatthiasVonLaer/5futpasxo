3ab6c6c9199af06546a3ab786390b9a673be3a91
Matthias <matthias-vonlaer@web.de>
2015-05-04 21:34:41 +0200

    dosiero

---
+++ bazregulverko/sorcxlistoj/magiisto_sorcxisto.php
@@ -0,0 +1,22 @@
+<?php require '../../pagxkapo.php' ?>
+
+<title>Sorĉoj de magiisto/sorĉisto</title>
+
+<h1>Sorĉoj de magiisto/sorĉisto</h1>
+
+<h2>Sorĉoj de magiisto/sorĉisto de la grado 0</h2>
+
+<div class="rubriko">Anatemo</div>
+<ul>
+    <li><a href="rezisto.php">Rezisto</a>: la celo ricevas +1 por savĵetoj.</li>
+</ul>
+<div class="rubriko">Vokado</div>
+<ul>
+    <li><a href="acidosxprucoj.php">Acidoŝprucoj</a>: kuglo kaŭzas 1k3 acidodamaĝon.</li>
+</ul>
+<div class="rubriko">Ekkono</div>
+<ul>
+    <li><a href="malkovri-veneon.php">Malkovri venenon</a>: malkovras venenon en kreaĵo aŭ malgranda objekto.</li>
+</ul>
+
+<?php require '../../pagxpiedo.php' ?>
