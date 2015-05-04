a768af456620a49c1ba6c37757c93721c0df3d0d
matti <matthias-vonlaer@web.de>
2015-05-05 00:36:24 +0200

    sorĉoj

--- bazregulverko/sorcxlistoj/magiisto_sorcxisto.php
+++ bazregulverko/sorcxlistoj/magiisto_sorcxisto.php
@@ -4,19 +4,145 @@
 
 <h1>Sorĉoj de magiisto/sorĉisto</h1>
 
-<h2>Sorĉoj de magiisto/sorĉisto de la grado 0</h2>
+<table>
+    <caption>Sorĉoj de magiisto/sorĉisto de la 0a grado</caption>
+    <tr class="nova-sekcio">
+         <td colspan="2">Anatemo</td>
+    </tr>
+    <tr>
+        <td><a href="rezisto.php">Rezisto</a></td>
+        <td>La celo ricevas +1 por savĵetoj.</td>
+    </tr>
+    <tr class="nova-sekcio">
+        <td colspan="2">Ekkono</td>
+    </tr>
+    <tr>
+        <td><a href="legi_magion.php">Legi magion</a></td>
+        <td>Vi povas legi skribaĵorulon kaj sorĉolibrojn.</td>
+    </tr>
+    <tr>
+        <td><a href="malkovri_magion.php">Malkovri magion</a></td>
+        <td>Malkovras sorĉojn kaj magiajn objektojn ene de 18 metroj.</td>
+    </tr>
+    <tr>
+        <td><a href="malkovri_veneon.php">Malkovri venenon</a></td>
+        <td>Malkovras venenon en kreaĵo aŭ malgranda objekto.</td>
+    </tr>
+    <tr class="nova-sekcio">
+        <td colspan="2">Elvokado</td>
+    </tr>
+    <tr>
+        <td><a href="dancantaj_lumoj.php">Dancantaj lumoj</a></td>
+        <td>Kreas torĉojn kaj aliajn lumojn.</td>
+    </tr>
+    <tr>
+        <td><a href="fulmo.php">Fulmo</a></td>
+        <td>Blindetigi kreaĵon (-1 por atakĵetoj).</td>
+    </tr>
+    <tr>
+        <td><a href="malvarma_strio.php">Malvarma strio</a></td>
+        <td>Strio kaŭzas 1k3 malvarmodamaĝo.</td>
+    </tr>
+    <tr>
+        <td><a href="lumo.php">Lumo</a></td>
+        <td>Objekto lumas kiel torĉo.</td>
+    </tr>
+    <tr class="nova-sekcio">
+        <td colspan="2">Iluzio</td>
+    </tr>
+    <tr>
+        <td><a href="fantoma_sono.php">Fantoma sono</a></td>
+        <td>Erariga sono.</td>
+    </tr>
+    <tr class="nova-sekcio">
+        <td colspan="2">Nekromancio</td>
+    </tr>
+    <tr>
+        <td><a href="elcxerpa_tusxo.php">Elĉerpa tuŝo</a></td>
+        <td>Tuŝatako elĉerpas la celon.</td>
+    </tr>
+    <tr>
+        <td><a href="elsangigi.php">Elsangigi</a></td>
+        <td>Igas stabilan kreaĵon morti.</td>
+    </tr>
+    <tr>
+        <td><a href="malfortigi_malmortulojn.php">Malfortigi malmortulojn</a></td>
+        <td>Kaŭzas 1k6 damaĝon ĉe malmortulon.</td>
+    </tr>
+    <tr class="nova-sekcio">
+        <td colspan="2">Sorĉvenigo</td>
+    </tr>
+    <tr>
+        <td><a href="acidosxprucoj.php">Acidoŝprucoj</a></td>
+        <td>Kuglo kaŭzas 1k3 acidodamaĝon.</td>
+    </tr>
+    <tr class="nova-sekcio">
+        <td colspan="2">Transformado</td>
+    </tr>
+    <tr>
+        <td><a href="fermi_malfermi.php">Fermi/malfermi</a></td>
+        <td>Fermas aŭ malfermas malgrandajn aŭ malpezajn objektojn.</td>
+    </tr>
+    <tr>
+        <td><a href="magia_mano.php">Magia mano</a></td>
+        <td>Telekinezo ĝis 2,5 kg.</td>
+    </tr>
+    <tr>
+        <td><a href="mesagxo.php">Mesaĝo</a></td>
+        <td>Flustrata interparolado distance.</td>
+    </tr>
+    <tr>
+        <td><a href="plibonigi.php">Plibonigi</a></td>
+        <td>Malgrandaj reparaturoj ĉe objekto.</td>
+    </tr>
+    <tr class="nova-sekcio">
+        <td colspan="2">Transsorĉado</td>
+    </tr>
+    <tr>
+        <td><a href="duonsvenigo.php">Duonsvenigo</a></td>
+        <td>Humanoide kreaĵo kun 4 aŭ malpli VK perdas sian venontan agon.</td>
+    </tr>
+    <tr class="nova-sekcio">
+        <td colspan="2">Universalaj</td>
+    </tr>
+    <tr>
+        <td><a href="arkana_sigelo.php">Arkana sigelo</a></td>
+        <td>Skribas vian personan runon sur viktimo aŭ kreaĵo (videble aŭ nevideble).</td>
+    </tr>
+    <tr>
+        <td><a href="sorcxtruko.php">Sorĉtruko</a></td>
+        <td>Malgrandaj magiaj trukoj.</td>
+    </tr>
+</table>
 
-<div class="rubriko">Anatemo</div>
-<ul>
-    <li><a href="rezisto.php">Rezisto</a>: la celo ricevas +1 por savĵetoj.</li>
-</ul>
-<div class="rubriko">Vokado</div>
-<ul>
-    <li><a href="acidosxprucoj.php">Acidoŝprucoj</a>: kuglo kaŭzas 1k3 acidodamaĝon.</li>
-</ul>
-<div class="rubriko">Ekkono</div>
-<ul>
-    <li><a href="malkovri-veneon.php">Malkovri venenon</a>: malkovras venenon en kreaĵo aŭ malgranda objekto.</li>
-</ul>
-
+<table>
+    <caption>Sorĉoj de magiisto/sorĉisto de la 1a grado</caption>
+    <tr class="nova-sekcio">
+         <td colspan="2">Anatemo</td>
+    </tr>
+    <tr class="nova-sekcio">
+        <td colspan="2">Ekkono</td>
+    </tr>
+    <tr class="nova-sekcio">
+        <td colspan="2">Elvokado</td>
+    </tr>
+    <tr class="nova-sekcio">
+        <td colspan="2">Iluzio</td>
+    </tr>
+    <tr class="nova-sekcio">
+        <td colspan="2">Nekromancio</td>
+    </tr>
+    <tr class="nova-sekcio">
+        <td colspan="2">Sorĉvenigo</td>
+    </tr>
+    <tr class="nova-sekcio">
+        <td colspan="2">Transformado</td>
+    </tr>
+    <tr class="nova-sekcio">
+        <td colspan="2">Transsorĉado</td>
+    </tr>
+    <tr class="nova-sekcio">
+        <td colspan="2">Universalaj</td>
+    </tr>
+</table>
 <?php require '../../pagxpiedo.php' ?>
